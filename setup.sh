3fde53907e5be5e077d902fe1a437af95ad8cd7b
Josh Jacobs <josh@joshjacobs.net>
2019-07-02 08:09:47 +0100

    Completed Initial Debian Install
    Needs testing and OS detection to add CentOS  support

--- setup.sh
+++ setup.sh
@@ -1,2 +1,42 @@
 #!/bin/bash
 
+# Update/Upgrade
+sudo apt update && sudo apt upgrade -y
+
+# Install useful packages
+sudo apt install python3 python3-pip git curl zsh screen tmux htop iftop tshark gpg mosh -y
+
+# Useful pip installs
+sudo pip3 install requests twisted beautifulsoup4 scapy scrapy ipython
+
+# Install ZeroTier (the secure way)
+curl -s 'https://raw.githubusercontent.com/zerotier/download.zerotier.com/master/htdocs/contact%40zerotier.com.gpg' | gpg --import && \
+if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
+
+# Join My ZeroTier Network
+sudo zerotier-cli join 12ac4a1e719def45
+
+# Clone dotfiles repo
+git clone https://github.com/biodrone/dotfiles ~/dotfiles
+
+# Copy tmux config
+cp ~/dotfiles/.tmux.conf ~
+
+# Grab OhMyZshell
+curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+sh install.sh --unattended
+
+# Copy OMZ config
+rm ~/.zshrc
+cp ~/dotfiles/.zshrc ~
+source ~/.zshrc
+
+# Clean up files/dirs
+rm -R ~/dotfiles
+rm install.sh
+
+# Change shell to zsh
+chsh -s $(which zsh)
+
+# Reboot
+sudo reboot
