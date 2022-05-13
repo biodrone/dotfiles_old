f1c2f23a5a2c5aae6cf3159cd9e14a52c758602e
Josh J <josh@joshjacobs.net>
2022-05-13 15:43:32 +0100

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+## Fix SSH Agent
+fish_ssh_agent
+
+## Aliases
+alias dcup="docker-compose up -d"
+alias dcpull="docker-compose pull"
+alias dcdown="docker-compose down"
+alias dcbuild="docker-compose build"
+alias dcrebuild="docker-compose down && docker-compose up -d"
+alias dclogs="docker-compose logs -f"
+alias l="ls -lah"
